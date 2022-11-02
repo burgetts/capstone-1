bb70953b6d851188a864a890e1feb3a5dafd6409
Stevie Burgett <stevieburgett@Stevies-MacBook-Pro.local>
2022-11-02 17:14:18 -0600

    changed database uri

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CURR_USER_KEY = 'curr_user'
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///bei'))
+    os.environ.get('DATABASE_URL', 'postgresql://bei'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
