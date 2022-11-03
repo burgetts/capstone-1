6672c30b686b07a18f4a46a966c51bd9b1fd9626
Stevie Burgett <stevieburgett@Stevies-MacBook-Pro.local>
2022-11-02 20:21:41 -0600

    uri change

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ CURR_USER_KEY = 'curr_user'
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql://bei'))
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
